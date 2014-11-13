953d483a13164770a57a831e42b24fcc15983da8
Takuto Wada <takuto.wada@gmail.com>
2014-11-13 11:40:29 +0900

    feat(espower-cli): `--config` or `-c` option to specify configuration JSON file

--- bin/cmd.js
+++ bin/cmd.js
@@ -14,16 +14,33 @@
 var transform = require('espower-source');
 var concat = require('concat-stream');
 var fs = require('fs');
-var file = process.argv[2];
-var input, filepath;
 
+var minimist = require('minimist');
+var argv = minimist(process.argv.slice(2), {
+    string: [
+        'config'
+    ],
+    alias: {
+        c: 'config'
+    },
+    default: {
+        config: null
+    }
+});
+
+var file = argv._[0];
+var input, filepath, config;
 if (file && file !== '-') {
-    // filepath = fs.realpathSync(file);
     filepath = file;
     input = fs.createReadStream(file);
 } else {
     input = process.stdin;
 } 
+
+if (argv.config) {
+    config = JSON.parse(fs.readFileSync(argv.config, 'utf8'));
+}
+
 input.pipe(concat(function(buf) {
-    console.log(transform(buf.toString('utf8'), filepath));
+    console.log(transform(buf.toString('utf8'), filepath, config));
 }));
--- test/espower_cli_test.js
+++ test/espower_cli_test.js
@@ -18,4 +18,35 @@ describe('espower-cli', function () {
             done();
         }));
     });
+    describe('config option', function () {
+        beforeEach(function () {
+            this.executable = path.join(__dirname, '..', 'bin', 'cmd.js');
+            var expected = fs.readFileSync('test/expected/customized.js', 'utf8');
+            this.assertOutput = function (args, done) {
+                var proc = child.spawn(this.executable, args);
+                proc.stdout.pipe(concat(function (output) {
+                    assert.equal(output.toString('utf8'), expected);
+                    done();
+                }));
+            };
+        });
+        it('--config option takes JSON file', function (done) {
+            this.assertOutput(['--config', 'test/customized_config.json', 'test/fixtures/customized.js'], done);
+        });
+        it('--config option at the end of arguments', function (done) {
+            this.assertOutput(['test/fixtures/customized.js', '--config', 'test/customized_config.json'], done);
+        });
+        it('short option -c', function (done) {
+            this.assertOutput(['-c', 'test/customized_config.json', 'test/fixtures/customized.js'], done);
+        });
+        it('-c option at the end of arguments', function (done) {
+            this.assertOutput(['test/fixtures/customized.js', '-c', 'test/customized_config.json'], done);
+        });
+        it('--config=JSONfile style', function (done) {
+            this.assertOutput(['--config=test/customized_config.json', 'test/fixtures/customized.js'], done);
+        });
+        it('--config=JSONfile at the end of arguments', function (done) {
+            this.assertOutput(['test/fixtures/customized.js', '--config=test/customized_config.json'], done);
+        });
+    });
 });
